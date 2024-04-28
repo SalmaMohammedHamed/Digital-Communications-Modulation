f51b6bf703233ccc4b533b05fb232e6541d53150
SalmaMohammedHamed <your-email@example.com>
2024-04-28 16:44:33 +0300

    Compelete Mapper & Demapper

--- Project3.m
+++ Project3.m
@@ -10,12 +10,27 @@ function DataStreamArr = DataCreation(NumOfBits)
     DataStreamArr = randi([0 1], 1, NumOfBits);
 end
 
+
 % Description:
 %map the bits to the corresponding symbols 
 function MappedArr = Mapper(ModulationType,DataStream, Positions,NumOfSymbols)
-    if ModulationType == 1
+    BPSK = 1;
+    PSK8 = 3;
+    QPSK = 2;
+    QAM16 = 4;
+    BFSK=5;
+    if ModulationType == BPSK
         MappedArr = DataStream * 2 - 1;
-    else
+    elseif ModulationType==BFSK
+        MappedArr = zeros(1,NumOfSymbols);
+        for j = 1:NumOfSymbols
+            if DataStream(j)==1
+                MappedArr(j)=Positions(1);
+            elseif DataStream(j)==0
+                MappedArr(j)=Positions(2);
+            end
+        end
+    elseif ModulationType== PSK8 || ModulationType==QPSK || ModulationType==QAM16
         MappedArr = zeros(1,NumOfSymbols);
         
         for j = 1:ModulationType:length(DataStream)
@@ -23,12 +38,15 @@ function MappedArr = Mapper(ModulationType,DataStream, Positions,NumOfSymbols)
             decimal_value = bin2dec(num2str(symbol)); % getting the decimal value of the symbol
             MappedArr(1,fix(j/ModulationType)+1) = Positions(1,decimal_value+1); % map to the corresponding symbol
         end
+    else 
+        error("Wronge Moduation Type");
      end
 end
 
+
+
+
 % Description:
-% Define the demapper function (get the data Stream from the received symbols)
-% NOTE: After creating the channel, replace every mapped data by the mapped data after the channel
 % Define the demapper function (get the data Stream from the received symbols)
 % NOTE: After creating the channel, replace every mapped data by the mapped data after the channel
 function DeMappedArr = DeMapper(ModulationType, RecievedArr,SymbolsArr,NumOfBits)
@@ -37,6 +55,7 @@ function DeMappedArr = DeMapper(ModulationType, RecievedArr,SymbolsArr,NumOfBits
     PSK8 = 3;
     QPSK = 2;
     QAM16 = 4;
+    BFSK=5;
     DeMappedArr = zeros(1,NumOfBits);
     NumOfSymbols = NumOfBits/ModulationType;
     % Perform demapping based on the modulation type
@@ -121,15 +140,26 @@ function DeMappedArr = DeMapper(ModulationType, RecievedArr,SymbolsArr,NumOfBits
             end
             DeMappedArr((j - 1) * ModulationType + 1 : j * ModulationType) = TempList;
         end
+        
+    elseif ModulationType==BFSK
+        Angle=pi/4;
+        for j = 1 : length(RecievedArr)
+            if (angle(RecievedArr(j)) < Angle && angle(RecievedArr(j))> -Angle)
+            DeMappedArr(j) = SymbolsArr(1);
+            else
+            DeMappedArr(j) = SymbolsArr(2);
+            end
+        end
     end
-    
-    
+     
 end
+
+
 
 
 % Description:
 % Entry point for the code's main logic
-function main ()
+function main
     NumOfBits=240000;
     %number of bit represents every modulation type
     BPSK = 1;
@@ -141,10 +171,9 @@ function main ()
 
     
     % Define symbols positions for QPSK modulation
-    
     BPSK_Positions=[1 ,-1];
     QPSK_Positions = [-1-1i , -1+1i , 1-1i , 1+1i];
-    QPSK_NotGreyCodePositions = [-1-1i , -1+1i , 1+1i , 1-1i];
+    QPSK_NotGreyCodedPositions = [-1-1i , -1+1i , 1+1i , 1-1i];
     % Define symbols positions for 16-QAM modulation
     QAM16_Positions = [-3-3i , -3-1i , -3+3i , -3+1i , -1-3i , -1-1i , -1+3i , -1+1i , ...
                        3-3i , 3-1i , 3+3i , 3+1i , 1-3i , 1-1i , 1+3i , 1+1i];
@@ -159,14 +188,17 @@ function main ()
     PSK8_Positions(6)=0.0 - 1.0i;
     PSK8_Positions(7)=-1.0 + 0.0i;
     PSK8_Positions(8)=-0.707106781186547 -0.707106781186548i;
+    %symbol positions for BFSK 
+    BFSK_Positions=[ 1.0+0.0i,0.0+1.0i];
 
 
     %demapping arrays (every region equivalent symbol)
     BPSK_SympolsArr=[1 0];
     QPSK_SympolsArr=[[1 1]; [0 1]; [1 0]; [0 0]];
+    QPSK_NotGreyCodedSympolsArr=[[1 0]; [0 1]; [1 1]; [0 0]];
     PSK8_SympolsArr=[[0 0 0];[0 0 1];[0 1 1];[0 1 0];[1 1 0];[1 1 1];[1 0 1];[1 0 0];];
     QAM16_SympolsArr=[[1 0];[1 1];[0 1];[0 0]];
-
+    BFSK_SympolsArr=[1 0];
     
     
     BPSK_DataStream = DataCreation(NumOfBits);
@@ -178,6 +210,12 @@ function main ()
     QPSK_Mapped = Mapper(QPSK,QPSK_DataStream,QPSK_Positions,NumOfBits/QPSK);
     QPSK_DeMapped = DeMapper(QPSK,QPSK_Mapped,QPSK_SympolsArr,NumOfBits);
     result=isequal(QPSK_DataStream,QPSK_DeMapped);
+    disp(result);
+    
+    QPSK_NotGreyCodedDataStream = DataCreation(NumOfBits);
+    QPSK_NotGreyCodedMapped = Mapper(QPSK,QPSK_NotGreyCodedDataStream,QPSK_NotGreyCodedPositions,NumOfBits/QPSK);
+    QPSK_NotGreyCodedDeMapped = DeMapper(QPSK,QPSK_NotGreyCodedMapped,QPSK_NotGreyCodedSympolsArr,NumOfBits);
+    result=isequal(QPSK_NotGreyCodedDataStream,QPSK_NotGreyCodedDeMapped);
     disp(result);
     
     PSK8_DataStream = DataCreation(NumOfBits);
@@ -192,4 +230,9 @@ function main ()
     QAM16_DeMapped = DeMapper(QAM16,QAM16_Mapped,QAM16_SympolsArr,NumOfBits);
     disp(isequal(QAM16_DataStream, QAM16_DeMapped));
     
+
+    BFSK_DataStream = DataCreation(NumOfBits);
+    BFSK_Mapped = Mapper(BFSK,BFSK_DataStream,BFSK_Positions,NumOfBits);
+    BFSK_DeMapped = DeMapper(BFSK,BFSK_Mapped,BFSK_SympolsArr,NumOfBits);
+    disp(isequal(BFSK_DataStream, BFSK_DeMapped));
 end
